d2ced6cae887b105174935e88a8f2cb7311faea8
Marcos Santos <marcosv.santos@gruposbf.com.br>
2023-11-07 14:11:28 -0300

    chore(addons): find addon causing package hoisting

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,8 +1,12 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
-  // Required
   framework: '@storybook/nextjs',
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-mdx-gfm',
+     //'@storybook/addon-essentials', this is causing package hoisting
+  ],
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   docs: {
     autodocs: 'tag',
